2961bac5fc6992f9f07f27351cdf9735883a83fd
Venkata Jaswanth <jaswanth@thoughtworks.com>
2016-03-15 21:24:15 +0530

    Jaswanth | Added link option for dev setup

--- bahmni.sh
+++ bahmni.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 case $@ in
-    init)
+    setup)
         docker run -d -p 443:443 -p 8080:8080 -p 8069:8069 -p 8081:8081 --name bahmni jaswanth/bahmni-docker
         ;;
-    init-dev)
+    setup-dev)
         docker run -d -p 127.0.0.1:443:443 -p 127.0.0.1:3306:3306 -p 127.0.0.1:5432:5432 -p 127.0.0.1:8080:8080 -p 127.0.0.1:8069:8069 -p 127.0.0.1:8081:8081 -p 127.0.0.1:8000:8000 \
         --name bahmni -v ~/bahmni-code:/bahmni-code:ro jaswanth/bahmni-docker
         ;;
@@ -17,11 +17,19 @@ case $@ in
     stop)
         docker stop bahmni
         ;;
-    destroy)
+    delete)
         docker rm -f bahmni
+        ;;
+    shell)
+        docker exec -it bahmni bash
+        ;;
+    link)
+        docker exec -it bahmni /bahmni-code/openmrs-module-bahmniapps/scripts/docker-link.sh
+        docker exec -it bahmni /bahmni-code/bahmni-core/scripts/docker-link.sh
+        docker exec -it bahmni /bahmni-code/default-config/scripts/docker-link.sh
         ;;
     *)
         echo "Invalid option";
-        echo "Available options : init, init-dev, start, status, stop, destroy";
+        echo "Available options : setup, setup-dev, start, status, stop, delete, shell, link";
         ;;
 esac
