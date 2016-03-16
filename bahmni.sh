54bb47391ced2cd4ebf37d31b7a8d07ea9fdd368
Venkata Jaswanth <jaswanth@thoughtworks.com>
2016-03-16 16:34:54 +0530

    Jaswanth | Add version to util script

--- bahmni.sh
+++ bahmni.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+VERSION=0.80
+
 case $@ in
     setup)
-        docker run -d -p 443:443 -p 8080:8080 -p 8069:8069 -p 8081:8081 --name bahmni jaswanth/bahmni-docker
+        docker run -d -p 443:443 -p 8080:8080 -p 8069:8069 -p 8081:8081 --name bahmni jaswanth/bahmni-docker:$VERSION
         ;;
     setup-dev)
         docker run -d -p 443:443 -p 3306:3306 -p 5432:5432 -p 8080:8080 -p 8069:8069 -p 8081:8081 -p 8000:8000 \
-        --name bahmni -v ~/bahmni-code:/bahmni-code:ro jaswanth/bahmni-docker
+        --name bahmni -v ~/bahmni-code:/bahmni-code:ro jaswanth/bahmni-docker:$VERSION
         ;;
     start)
         docker start bahmni
