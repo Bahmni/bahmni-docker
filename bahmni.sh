33f8c0f19b530cab8ea6aa9ad071644680c7e7e6
Venkata Jaswanth <jaswanth@thoughtworks.com>
2016-03-16 10:33:33 +0530

    Jaswanth | Remove binding on localhost because of non-linux machines

--- bahmni.sh
+++ bahmni.sh
@@ -5,7 +5,7 @@ case $@ in
         docker run -d -p 443:443 -p 8080:8080 -p 8069:8069 -p 8081:8081 --name bahmni jaswanth/bahmni-docker
         ;;
     setup-dev)
-        docker run -d -p 127.0.0.1:443:443 -p 127.0.0.1:3306:3306 -p 127.0.0.1:5432:5432 -p 127.0.0.1:8080:8080 -p 127.0.0.1:8069:8069 -p 127.0.0.1:8081:8081 -p 127.0.0.1:8000:8000 \
+        docker run -d -p 443:443 -p 3306:3306 -p 5432:5432 -p 8080:8080 -p 8069:8069 -p 8081:8081 -p 8000:8000 \
         --name bahmni -v ~/bahmni-code:/bahmni-code:ro jaswanth/bahmni-docker
         ;;
     start)
