199c5eb1daa75cf3d43d4666d8ff55ecee55c103
_n0man <nouman.memon@icloud.com>
2022-12-15 18:03:26 +0530

    generate seperate architecture diagram for lite and standard

--- generate_architecture_diagram.sh
+++ generate_architecture_diagram.sh
@@ -2,7 +2,9 @@ echo "Running script to generate architecture diagram from docker-compose..."
 
 #For generating architecture diagram with networks, ports and volumes
 if
-  docker run --rm -it --name bahpkg -v ${PWD}:/input pmsipilot/docker-compose-viz:latest  render -m image --force docker-compose.yml --output-file=architecture-diagram/docker-compose-architecture-diagram.png $1 $2 $3
+  docker run --rm -it --name bahpkg -v ${PWD}:/input pmsipilot/docker-compose-viz:latest  render -m image --force bahmni-lite/docker-compose.yml --output-file=architecture-diagram/bahmni-lite-architecture-diagram.png $1 $2 $3
+
+  docker run --rm -it --name bahpkg -v ${PWD}:/input pmsipilot/docker-compose-viz:latest  render -m image --force bahmni-standard/docker-compose.yml --output-file=architecture-diagram/bahmni-standard-architecture-diagram.png $1 $2 $3
 then
   echo "Successfully generated architecture diagram from docker-compose!"
 else
