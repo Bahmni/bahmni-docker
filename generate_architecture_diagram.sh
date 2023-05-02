d85248116a8f31ada0aafbb9b04c2d83befc7117
Umair Fayaz <omayrfayaz@gmail.com>
2023-05-02 14:31:38 +0530

    Updated the generate architecture diagram script

--- generate_architecture_diagram.sh
+++ generate_architecture_diagram.sh
@@ -2,9 +2,7 @@ echo "Running script to generate architecture diagram from docker-compose..."
 
 #For generating architecture diagram with networks, ports and volumes
 if
-  docker run --rm -it --name bahpkg -v ${PWD}:/input pmsipilot/docker-compose-viz:latest  render -m image --force bahmni-lite/docker-compose.yml --output-file=architecture-diagram/bahmni-lite-architecture-diagram.png $1 $2 $3
-
-  docker run --rm -it --name bahpkg -v ${PWD}:/input pmsipilot/docker-compose-viz:latest  render -m image --force bahmni-standard/docker-compose.yml --output-file=architecture-diagram/bahmni-standard-architecture-diagram.png $1 $2 $3
+  docker run --rm -it --name bahpkg -v ${PWD}:/input pmsipilot/docker-compose-viz:latest  render -m image --force bahmni/docker-compose.yml --output-file=architecture-diagram/bahmni-architecture-diagram.png $1 $2 $3
 then
   echo "Successfully generated architecture diagram from docker-compose!"
 else
