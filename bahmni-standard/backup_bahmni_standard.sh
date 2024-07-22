459262522644d8611fc7033c0fab370e2416a141
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2024-07-22 18:45:11 +0530

    BAH-4038 | Add. Backup and restore commands for Odoo 10 database (#91)
    * BAH-4038 | Add. Extend backup restore script to support Odoo-10
    
    * BAH-4038 | Fix. Service name for Odoo 10 DB

--- bahmni-standard/backup_bahmni_standard.sh
+++ bahmni-standard/backup_bahmni_standard.sh
@@ -22,6 +22,7 @@ openmrs_db_backup_file_path=$backup_subfolder_path/openmrsdb_backup.sql
 reports_db_backup_file_path=$backup_subfolder_path/reportsdb_backup.sql
 openelis_db_backup_file_path=$backup_subfolder_path/openelisdb_backup.sql
 odoo_db_backup_file_path=$backup_subfolder_path/odoodb_backup.sql
+odoo_10_db_backup_file_path=$backup_subfolder_path/odoo_10_db_backup.sql
 dcm4chee_db_backup_file_path=$backup_subfolder_path/dcm4cheedb_backup.sql
 pacs_integration_db_backup_file_path=$backup_subfolder_path/pacs_integrationdb_backup.sql
 
@@ -32,7 +33,9 @@ dcm4chee_service_name="dcm4chee"
 openmrs_db_service_name="openmrsdb"
 reports_db_service_name="reportsdb"
 openelis_db_service_name="openelisdb"
+odoo_service_name="odoo"
 odoo_db_service_name="odoodb"
+odoo_10_db_service_name="odoo-10-db"
 dcm4chee_db_service_name="pacsdb"
 pacs_integration_db_service_name="pacsdb"
 
@@ -47,6 +50,9 @@ backup_db "postgres" "clinlims" $OPENELIS_DB_USER $OPENELIS_DB_PASSWORD $openeli
 
 log_info "Taking backup for Odoo Database"
 backup_db "postgres" $ODOO_DB_NAME $ODOO_DB_USER $ODOO_DB_PASSWORD $odoo_db_service_name $odoo_db_backup_file_path
+
+log_info "Taking backup for Odoo 10 Database"
+backup_db "postgres" $ODOO_10_DB_NAME $ODOO_10_DB_USER $ODOO_10_DB_PASSWORD $odoo_10_db_service_name $odoo_10_db_backup_file_path
 
 log_info "Taking backup for DCM4CHEE Database"
 backup_db "postgres" $DCM4CHEE_DB_NAME $DCM4CHEE_DB_USERNAME $DCM4CHEE_DB_PASSWORD $dcm4chee_db_service_name $dcm4chee_db_backup_file_path
@@ -75,6 +81,9 @@ backup_container_file_system $openmrs_service_name "/openmrs/data/configuration_
 
 log_info "Taking backup for Queued Reports results"
 backup_container_file_system $reports_service_name "/home/bahmni/reports" "$BACKUP_ROOT_FOLDER"
+
+log_info "Taking backup for Odoo Files"
+backup_container_file_system $odoo_service_name "/var/lib/odoo/filestore" "$BACKUP_ROOT_FOLDER"
 
 log_info "Taking backup for DCM4CHEE Archive"
 backup_container_file_system $dcm4chee_service_name "/var/lib/bahmni/dcm4chee/server/default/archive/." "$BACKUP_ROOT_FOLDER/dcm4chee_archive"
--- bahmni-standard/restore_bahmni_standard.sh
+++ bahmni-standard/restore_bahmni_standard.sh
@@ -25,6 +25,7 @@ openmrs_db_service_name="openmrsdb"
 reports_db_service_name="reportsdb"
 openelis_db_service_name="openelisdb"
 odoo_db_service_name="odoodb"
+odoo_10_db_service_name="odoo-10-db"
 dcm4chee_db_service_name="pacsdb"
 pacs_integration_db_service_name="pacsdb"
 
@@ -33,6 +34,7 @@ openmrs_db_backup_file_path="${RESTORE_ARTIFACTS_PATH}/openmrsdb_backup.sql"
 reports_db_backup_file_path="${RESTORE_ARTIFACTS_PATH}/reportsdb_backup.sql"
 openelis_db_backup_file_path="${RESTORE_ARTIFACTS_PATH}/openelisdb_backup.sql"
 odoo_db_backup_file_path="${RESTORE_ARTIFACTS_PATH}/odoodb_backup.sql"
+odoo_10_db_backup_file_path="${RESTORE_ARTIFACTS_PATH}/odoo_10_db_backup.sql"
 dcm4chee_db_backup_file_path="${RESTORE_ARTIFACTS_PATH}/dcm4cheedb_backup.sql"
 pacs_integration_db_backup_file_path="${RESTORE_ARTIFACTS_PATH}/pacs_integrationdb_backup.sql"
 
@@ -46,6 +48,8 @@ restore_db "mysql" $REPORTS_DB_NAME root $MYSQL_ROOT_PASSWORD $reports_db_servic
 restore_db "postgres" "clinlims" $OPENELIS_DB_USER $OPENELIS_DB_PASSWORD $openelis_db_service_name $openelis_db_backup_file_path
 
 restore_db "postgres" $ODOO_DB_NAME $ODOO_DB_USER $ODOO_DB_PASSWORD $odoo_db_service_name $odoo_db_backup_file_path
+
+restore_db "postgres" $ODOO_10_DB_NAME $ODOO_10_DB_USER $ODOO_10_DB_PASSWORD $odoo_10_db_service_name $odoo_10_db_backup_file_path
 
 restore_db "postgres" $DCM4CHEE_DB_NAME $DCM4CHEE_DB_USERNAME $DCM4CHEE_DB_PASSWORD $dcm4chee_db_service_name $dcm4chee_db_backup_file_path
 
