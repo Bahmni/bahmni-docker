9f53107c41ff8d5c3fdcf11f285b1abee87d7b27
Lethusang_Ko <kotjomelalethusang@gmail.com>
2025-11-27 12:52:51 +0200

    Prepacking init

---
+++ bahmni-standard/extra-odoo-addons/prepacking/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "prepacking",
+    "summary": "MOH Prepacking Customizations",
+    "version": "16.0.1.0.0",
+    "category": "Localization",
+    "author": "Your Name",
+    "website": "",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "contacts",
+        "sale",
+        "account",
+        "stock",
+        "mrp",
+    ],
+    "data": [
+        "views/menu_overrides.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
