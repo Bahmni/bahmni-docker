f3563f2563a8aaa86d8e0d1ea67eb0cf03013eab
PenaneMaime <penanemaime@gmail.com>
2025-11-28 09:48:14 +0200

    renamed bills of material to prepacking template

---
+++ bahmni-standard/extra-odoo-addons/lesotho_manufacturing/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "Lesotho Manufaturing",
+    "summary": "Lesotho Manufaturing",
+    "version": "16.0.1.0.0",
+    "category": "Localization",
+    "author": "MOH Lesotho",
+    "website": "",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "contacts",
+        "sale",
+        "account",
+        "stock",
+        "mrp",
+    ],
+    "data": [
+        "views/menu_overrides.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
