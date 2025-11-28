1938086d554a805550b6c88189e295d3fed150fd
lmphatsi <la.mphatsi@gmail.com>
2025-11-28 04:41:42 +0000

    rename Discuss to Notifications

---
+++ bahmni-standard/extra-odoo-addons/lesotho_mail/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "lesotho_mail",
+    "summary": "Lesotho Mail Customizations",
+    "version": "16.0.1.0.0",
+    "category": "Localization",
+    "author": "eLMIS Development Team",
+    "website": "",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "contacts",
+        "mail",
+        "account",
+    ],
+    "data": [
+        "views/menu_overrides.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
