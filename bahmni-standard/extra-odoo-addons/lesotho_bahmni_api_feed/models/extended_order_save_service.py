b769f1c2e1ffeff25ca1be404323701369ab32cd
Lethusang_Ko <kotjomelalethusang@gmail.com>
2026-03-23 01:52:50 +0200

    new edits to use using the extension of the odoo connect service

--- bahmni-standard/extra-odoo-addons/lesotho_bahmni_api_feed/models/extended_order_save_service.py
+++ bahmni-standard/extra-odoo-addons/lesotho_bahmni_api_feed/models/extended_order_save_service.py
@@ -54,6 +54,32 @@ class ExtendedOrderSaveService(models.Model):
         # We use .pop() so it doesn't cause errors down the line
         allergies_payload = vals.pop("patient_allergies_payload", None)
         # ========================================================
+        # ========================================================
+        # NEW: MERGE EXTENDED PRESCRIPTION DETAILS
+        # ========================================================
+        extended_prescriptions = vals.pop("extended_prescription_details", {})
+        if extended_prescriptions:
+            orders_data = vals.get("orders")
+            if orders_data:
+                # 1. Open the default orders data
+                if isinstance(orders_data, str):
+                    try:
+                        orders_dict = json.loads(orders_data)
+                    except Exception:
+                        orders_dict = {}
+                else:
+                    orders_dict = dict(orders_data)
+
+                # 2. Inject the clinical details right into the array!
+                if "openERPOrders" in orders_dict:
+                    for order in orders_dict["openERPOrders"]:
+                        order_uuid = order.get("orderId")
+                        if order_uuid and order_uuid in extended_prescriptions:
+                            order.update(extended_prescriptions[order_uuid])
+
+                # 3. Save the enriched array back into the payload
+                vals["orders"] = orders_dict
+        # ========================================================
 
         # You can stash them on vals for downstream use or write to a context key
         vals = dict(
--- bahmni-standard/extra-odoo-addons/lesotho_sale/models/__init__.py
+++ bahmni-standard/extra-odoo-addons/lesotho_sale/models/__init__.py
@@ -2,5 +2,6 @@ from . import (
     # extended_res_partner,
     extended_sale_order,
     extended_sale_order_line,
+    patient_allergy,
     res_partner,
 )
